cd08799fcf4d83b6ce9c57bf390d17283bb92406
maddytady <maddytady@gmail.com>
2023-01-14 19:18:48 +0500

    rdx

---
+++ src/store/actions.js
@@ -0,0 +1,19 @@
+import * as actionTypes from './actionTypes';
+
+export function taskCompleted(id) {
+  return { type: actionTypes.taskUpdated, payload: { id: id, completed: true } };
+}
+
+export function titleChanged(id) {
+  return {
+    type: actionTypes.taskUpdated,
+    payload: { id: id, title: `New title for ${id}` },
+  };
+}
+
+export function titleDeleted(id) {
+  return {
+    type: actionTypes.taskDeleted,
+    payload: { id },
+  };
+}
